4c03ce5b4a867717b74b99e47a39ab8e92b8f921
wrzhxy <wrzhxy@qq.com>
2017-08-01 21:53:48 +0800

    binary search recursion and loop

--- algorithm/BinarySearch/main.cpp
+++ algorithm/BinarySearch/main.cpp
@@ -16,15 +16,27 @@ const int SIZE = 20;
 int search_loop(int a[], int key) {
     int hi = SIZE - 1;
     int lo = 0;
-    int mid = (hi + lo) / 2;
+    int mid;
 
     // 遍历数据，查询关键词
-    while (lo <= mid) {
+    while (lo <= hi) {
+        mid = (hi + lo) / 2;
         if (key == a[mid]) return mid;
         else if (key < a[mid]) hi = mid - 1;
         else if (key > a[mid]) lo = mid + 1;
     }
     return -1;
+}
+
+/*
+    递归
+*/
+int search_recursion(int a[], int key, int lo, int hi) {
+    if (lo > hi) return -1;
+    int mid = (lo + hi) / 2;
+    if (a[mid] == key) return mid;
+    else if (a[mid] > key) return search_recursion(a, key, lo, mid - 1);
+    else if (a[mid] < key) return search_recursion(a, key, mid + 1, hi);
 }
 
 int main()
@@ -33,6 +45,10 @@ int main()
     for (int i = 0; i < SIZE; i++) {
         a[i] = 2 * i + 1;
     }
+    int result = search_recursion(a, 3, 0, SIZE - 1);
+
+    cout << result << endl;
+
 
 
     return 0;
