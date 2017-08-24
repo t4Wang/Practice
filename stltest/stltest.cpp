de93b99440d0ccd6314b2d42407c5af06685edf8
wrzhxy <wrzhxy@qq.com>
2017-08-24 18:54:09 +0800

    stl test update

--- stltest/stltest.cpp
+++ stltest/stltest.cpp
@@ -1,3 +1,4 @@
+/*
 一个整数集 , 该整数集可以有重复元素, 初始为空集合 。可以指定如下指令：
 1. add x 将x加入整数集
 2. del x 删除整数集中所有与x相等的元素
@@ -33,3 +34,66 @@ ask 1
 0
 2
 1 0
+*/
+#include <queue>
+
+#include <iostream>
+using namespace std;
+// 一个队列存储指令
+class InstructionSet {
+	// set<int> datas;
+	queue<pair<string, int> > msgs;
+	// 命令数
+public:
+	// InstructionSet()
+	void insertInstruct(string instruction, int data) { msgs.push(pair<string, int>(instruction, data)); }
+	void executeInstruct();
+};
+
+class InstructionSys {
+	int n;
+	InstructionSet instruct;
+public:
+	InstructionSys(const int n = 1): n(n) {}
+	int getN() const { return n; }
+	void receiveInstruct();
+	void executeInstruct();
+};
+inline void
+InstructionSys::receiveInstruct() {
+	string instruction;
+	int data;
+	// 循环执行命令
+	for(int i = 0; i < n; i++) {
+		// 接收指令
+		cin >> instruction >> data;
+		// 处理指令
+		instruct.insert(instruct, data);
+	}
+}
+inline void
+InstructionSys::executeInstruct() {
+	instruct.executeInstruct();
+}
+
+/*
+if ("add" == instruction) {
+			// 添加元素
+		} else if ("del" == instruction) {
+			// 删除元素
+		} else if ("ask" == instruction) {
+			// 查询元素
+		}
+*/
+
+int main() {
+	// 接受输入
+	int n = 0;	// 命令数
+	cin >> n;
+	if (0 > n || 100000 < n) return 0;
+
+	InstructionSys is(n);
+	
+	is.receiveInstruct();
+	// is.executeInstruct();
+}
